2727799beb1acd300845d797d5f7ecba3dc53ce7
SamMan56 <111656378+SamMan56@users.noreply.github.com>
2022-09-04 18:13:09 +0100

    now clears plot

--- src/main/kotlin/net/dfplots/templateplacer/TemplatePlacer.kt
+++ src/main/kotlin/net/dfplots/templateplacer/TemplatePlacer.kt
@@ -3,6 +3,11 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks
+import net.minecraft.client.gui.screen.ingame.GenericContainerScreen
+import net.minecraft.inventory.Inventory
+import net.minecraft.item.Item
+import net.minecraft.item.Items
+import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -35,6 +40,8 @@ object TemplatePlacer: ModInitializer {
                     }
                 }
 
+                fun Inventory.toList() = List(size(), this::getStack)
+
                 // TODO: add a check if they holding iron ingot
 
                 val borderBlock = Blocks.STONE
@@ -45,6 +52,66 @@ object TemplatePlacer: ModInitializer {
                 val east = Direction.SOUTH
                 val south = Direction.WEST
                 val west = Direction.NORTH
+
+                fun StepByStep.waitForInvToOpen(): Unit {
+                    tryTo {
+                        if (client.currentScreen == null)
+                            Result.KEEP_TRYING
+                        else Result.SUCCESS
+                    }
+                }
+
+                fun StepByStep.tryWithScreen(f: (GenericContainerScreen) -> Result) {
+                    tryTo {
+                        val currentScreen = client.currentScreen
+                        if (currentScreen != null && currentScreen is GenericContainerScreen) {
+                            f(currentScreen)
+                        }
+                        else Result.FAIL
+                    }
+                }
+
+                fun StepByStep.clickAndWait(slotId: Int) {
+                    tryWithScreen { genericContainerScreen ->
+                        if (genericContainerScreen.screenHandler.inventory.getStack(slotId).item == Items.AIR)
+                            return@tryWithScreen Result.FAIL
+
+                        client.interactionManager?.clickSlot(
+                            genericContainerScreen.screenHandler.syncId,
+                            slotId,
+                            0,
+                            SlotActionType.PICKUP,
+                            player
+                        )
+                        Result.SUCCESS
+                    }
+                    tryWithScreen { genericContainerScreen ->
+                        if (player.currentScreenHandler.cursorStack.item != Items.AIR)
+                            return@tryWithScreen Result.KEEP_TRYING // wait until server puts an item there
+                        Result.SUCCESS
+                    }
+                }
+
+                fun StepByStep.clickToClose(slotId: Int) {
+                    tryWithScreen { genericContainerScreen ->
+                        printToClient(genericContainerScreen.screenHandler.inventory.getStack(slotId).name.asString())
+                        if (genericContainerScreen.screenHandler.inventory.getStack(slotId).item == Items.AIR)
+                            return@tryWithScreen Result.FAIL
+
+                        client.interactionManager?.clickSlot(
+                            genericContainerScreen.screenHandler.syncId,
+                            slotId,
+                            0,
+                            SlotActionType.PICKUP,
+                            player
+                        )
+                        Result.SUCCESS
+                    }
+                    tryWithScreen { genericContainerScreen ->
+                        if (client.currentScreen != null) Result.KEEP_TRYING
+                        else Result.SUCCESS
+                    }
+                }
 
                 stepByStep {
                     tryTo { // initial burst of speed
@@ -67,6 +134,27 @@ object TemplatePlacer: ModInitializer {
                             Result.SUCCESS
                         else Result.KEEP_TRYING
                     }
+                    tryTo { // stop the player flying out of bounds
+                        player.velocity = Vec3d(0.0, 0.0, 0.0)
+
+                        Result.SUCCESS
+                    }
+                    tryTo {
+                        player.sendChatMessage("/plot clear")
+
+                        Result.SUCCESS
+                    }
+                    waitForInvToOpen()
+
+                    // configure clearing settings to plot
+                    clickAndWait(9)
+                    clickAndWait(14)
+                    clickAndWait(15)
+                    clickAndWait(16)
+                    clickAndWait(44)
+
+                    // press clear
+                    clickToClose(11)
                 }
 
                 1
